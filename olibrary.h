2cc657b7da12982555059983a9f45fffdd247b32
ykahil <ykahil@students.cs.ubc.ca>
2024-05-01 22:47:44 -0700

    Fixed up documentation on UI methods in olibrary

--- olibrary.h
+++ olibrary.h
@@ -17,10 +17,25 @@ private:
     Library& lib;
 
 protected:
+    /*
+     * Sets up the main tables column sizes to a reasonable size depending on 
+     * what they represent.
+     */
     void setupTableColumns();
+
+    /*
+     * Adjusts the size of the table when the window is resized depending
+     * on the size of the new window after being resized .
+     * 
+     * @param event - specifies the resize event
+     */
     void resizeEvent(QResizeEvent* event);
 
 private slots:
+    /*
+     * Displays all the books in the Library instance once the "View All Books" button is pressed, 
+     * seperates the details by there splits "|" and places them into their corresponding columns.
+     */
     void on_viewAllButton_clicked();
 
 
